8618d557620c7277df00aa68812802968ccc583b
ada <ada@cubething.dev>
2026-06-04 11:47:27 -0500

    refactor(process+shell): introduce ProgramLabel / Process / ForegroundProcessGroup model
    Replace the Process trait (Component + Default + Reflect) with a label-based
    program model:
    
    - ProgramLabel (via define_label!) identifies a program; interned as
      InternedProgramLabel.
    - Programs resource maps InternedProgramLabel -> ProgramData
      (HashMap<InternedScheduleLabel, SystemId<In<Process>, ()>>).
    - Process is now an immutable Component carrying entity, prog, argv, environ,
      signal_overrides, and fd0/fd1/fd2, with write/write_err helpers.
    - ProgramAppExt adds register_program and add_program_system on App.
    - run_programs::<S> + impl_run_progs! macro wires program systems into
      PreUpdate, Update, PostUpdate and the Fixed variants via plugin(app).
    - impl_program_label! macro for declaring labels until a derive exists.
    
    Shell becomes a struct { term: Entity } with a #[relationship] field and
    method into a ShellSpawnMsg + spawn_process system that builds a full Process
    from a shell + terminfo. ShellJob/ShellJobTarget relocate from process.rs to
    shell.rs, and ForegroundProcess is paired with a new ForegroundProcessGroup
    relationship target (Vec<Entity>) on the shell.

--- src/data/process.rs
+++ src/data/process.rs
@@ -1,87 +1,176 @@
 //! Basic data types required for process execution
-use bevy::ecs::schedule::ScheduleLabel;
+use bevy::{
+    ecs::{
+        define_label,
+        intern::Interned,
+        schedule::{InternedScheduleLabel, ScheduleLabel},
+        system::SystemId,
+    },
+    platform::collections::{HashMap, hash_map::Entry},
+};
 
 use crate::prelude::*;
 
 #[test]
-fn doctest_process() {
+fn example() {
     use crate::prelude::*;
     let mut app = App::new();
 
     #[derive(ScheduleLabel, Debug, Clone, Copy, PartialEq, Eq, Hash)]
     struct MySchedule;
 
-    #[derive(Component)]
-    #[component(on_add = Self::on_add, on_remove = Self::on_remove)]
-    struct MyProcess;
-    impl MyProcess {
-        fn on_add(mut world: DeferredWorld, ctx: HookCtx) {
-            info!("Initializing MyProcess");
-        }
-        fn on_remove(mut world: DeferredWorld, ctx: HookCtx) {
-            info!("Deinitializing MyProcess");
-        }
-    }
-    impl Process for MyProcess {
-        fn name() -> &'static str {
-            "my_process"
-        }
-        fn schedule_label() -> impl ScheduleLabel {
-            MySchedule
-        }
-    }
+    #[derive(Clone, Copy)]
+    struct MyProg;
+    impl_program_label!(MyProg, "myprog");
 
     let term = app.world_mut().spawn(Terminal).id();
     let shell = app.world_mut().spawn(Shell).id();
-    schedule.add_systems(move |mut commands: Commands, terminfo: Query<TermInfo>| {
-        let terminfo = terminfo.get(term).unwrap();
-        terminfo.write(&mut commands, "hi\n");
-    });
-    app.register_process::<MyProcess>();
+
+    app.register_program(MyProg);
+
+    // Say hello once every second
+    app.add_program_system(
+        MyProg,
+        Update,
+        |proc: In<Process>,
+         mut commands: Commands,
+         mut timer: Local<Option<Timer>>,
+         time: Res<Time>| {
+            if timer.is_none() {
+                timer = Some(Timer::from_seconds(1., TimerMode::Repeating)).into();
+            }
+            timer.as_ref().unwrap().tick(time.delta());
+            if timer.just_finished() {
+                proc.write(
+                    &mut commands,
+                    format!("Hello from process {}!", proc.entity),
+                );
+            }
+        },
+    );
+
     app.add_systems(Startup, |mut commands: Commands| {
-        Shell::spawn_process::<MyProcess>(shell, &mut commands);
+        Shell::spawn_process::<MyProg>(shell, &mut commands);
     });
 }
 
-/// A [`Process`] is a potentially long-running [`Schedule`] which is
-/// managed by a [`Shell`]. Processes should be run by running [`Shell::spawn_process`].
-/// The process dies when this component is removed.
-/// Lifecycle hooks are a good way to implement de/initialization behaviors.
-// TODO: inline doctest here
-// TODO: Process should have reference to its own entity.
-// Cannot assume singleton entity, multiple instances of the same processes
-// are likely to be spawned (job control).
-// TODO: Piping? Need file descriptors if so. Probably a relationship (ProcessFd<const CHANNEL: u8)
-pub trait Process: Component + Default + Reflect {
-    /// Name of the process, used to run it on the command line.
-    fn name() -> ProcessName;
-
-    /// The schedule assocaited with this [`Process`].
-    /// By default this schedule will run on [`Update`].
-    /// Override which step this runs on with [Self::runs_on].
-    fn schedule_label() -> impl ScheduleLabel;
-
-    /// Override to change which schedule this subschedule runs on.
-    /// Defaults to [`Update`]
-    fn runs_on() -> impl ScheduleLabel {
-        Update
+/// A [`Resource`] which tracks registered a [`Program`] through its
+/// [`ProgramLabel`].
+#[derive(Resource, Default, Deref, DerefMut)]
+pub struct Programs(HashMap<InternedProgramLabel, ProgramData>);
+impl Programs {
+    pub fn get(&self, label: impl ProgramLabel) -> Option<&ProgramData> {
+        self.0.get(&label.intern())
+    }
+    pub fn get_mut(&mut self, label: impl ProgramLabel) -> Option<&mut ProgramData> {
+        self.0.get_mut(&label.intern())
+    }
+    pub fn contains(&self, label: impl ProgramLabel) -> bool {
+        self.0.contains_key(&label.intern())
+    }
+    pub fn entry(
+        &mut self,
+        label: impl ProgramLabel,
+    ) -> Entry<'_, InternedProgramLabel, ProgramData> {
+        self.0.entry(label.intern())
     }
 }
 
-pub enum ProcessCommands {
-    /// Write to stdout. The pty systems will determine where this gets piped to.
-    StdOut { this: Entity, message: String },
-}
-impl Command for ProcessCommands {
-    fn apply(self, world: &mut World) {
-        match self {
-            ProcessCommands::StdOut { this, message } => {
-                let e = r!(world.get_entity(this));
-            }
+/// Data associated with a [`Program`]. Specifically, [`SystemId`]s mapped to [`ScheduleLabel`]s.
+/// Note that this currently only accepts **one** system per label.
+pub type ProgramData = HashMap<InternedScheduleLabel, SystemId<In<Process>, ()>>;
+
+define_label!(
+    /// A [`Process`] is a potentially long-running [`Schedule`] which is
+    /// managed by a [`Shell`]. Processes should be run by running [`Shell::spawn_process`].
+    /// The process dies when this component is removed.
+    /// Lifecycle hooks are a good way to implement de/initialization behaviors.
+    // TODO: inline doctest here
+    // TODO: Process should have reference to its own entity.
+    // Cannot assume singleton entity, multiple instances of the same processes
+    // are likely to be spawned (job control).
+    // TODO: Piping? Need file descriptors if so. Probably a relationship (ProcessFd<const CHANNEL: u8)
+    ProgramLabel,
+    PROGRAM_LABEL_INTERNER,
+    extra_methods: {
+        /// Name of the process, used to run it on the command line.
+        fn name(&self) -> ProcessName;
+    },
+    extra_methods_impl: {
+        /// Name of the process, used to run it on the command line.
+        fn name(&self) -> ProcessName {
+            ProcessName::new("PLACEHOLDER")
         }
     }
+);
+
+/// Shorthand for Interned<dyn ProgramLabel>
+pub type InternedProgramLabel = Interned<dyn ProgramLabel>;
+
+/// A [`Program`] is a set of instructions which is instantiated by spawning a
+/// [`Process`]. Where the [`Process`] is the [`Component`], this is the [`System`]
+/// manager.
+pub trait Program {
+    /// Signal overriding behavior.
+    /// Returns a HashMap from the signal to its override command.
+    /// By default, SIGINT, SIGQUIT, SIGTERM, and SIGHUP all despawn the entity.
+    /// Use [`ProcessSignalOverride`] to define the schedule.
+    fn trap(&self, _kind: Sig) -> Option<Schedule> {
+        None
+    }
 }
-impl ProcessCommands {}
+
+// TODO: Derive macro for ProgramLabel
+#[macro_export]
+macro_rules! impl_program_label {
+    ($t:ty, $name:literal) => {
+        impl ProgramLabel for $t {
+            fn name() -> ProcessName {
+                ProcessName::new($name)
+            }
+            fn dyn_clone(&self) -> Box<dyn ProgramLabel> {
+                Box::new(self.clone())
+            }
+        }
+    };
+}
+
+/// An instantiated process; a running program. Modify its behavior by
+/// implementing its [`ProgramLabel`]
+#[derive(Component, Clone)]
+#[component(immutable)]
+pub struct Process {
+    /// The currently attached entity ID.
+    /// Equivalent to UNIX pid.
+    pub entity: Entity,
+    /// The [`ProgramLabel`] associated with this [`Process`].
+    /// Determines what this process _does_.
+    pub prog: InternedProgramLabel,
+    /// Signal catching behavior
+    pub signal_overrides: HashMap<Sig, SystemId>,
+    /// Argument values.
+    pub argv: Vec<String>,
+    /// Environment variables.
+    pub environ: HashMap<String, String>,
+    /// stdin
+    pub fd0: Entity,
+    /// stdout
+    pub fd1: Entity,
+    /// stderr
+    pub fd2: Entity,
+}
+impl Process {
+    /// Writes to stdout (the entity at self.fd1)
+    pub fn write(&self, commands: &mut Commands, msg: impl ToString) {
+        let msg = StdOut::write(self.fd1, msg);
+        commands.write_message(msg);
+    }
+    /// Writes to stderr (the entity at self.fd2)
+    pub fn write_err(&self, commands: &mut Commands, msg: impl ToString) {
+        let msg = StdOut::write(self.fd2, msg);
+        commands.write_message(msg);
+    }
+}
 
 /// The name of a [`Process`]. This type exists to ensure validity on construction.
 /// In particular, process names must not contain whitespace.
@@ -98,31 +187,66 @@ impl ProcessName {
     }
 }
 
-/// Marker for a process owned by a [`Shell`].
-#[derive(Component, Reflect, Debug)]
-#[relationship(relationship_target = ShellJobTarget)]
-pub struct ShellJob(pub Entity);
+pub trait ProgramAppExt {
+    fn register_program(&mut self, prog: impl ProgramLabel);
+    // NOTE: This is similar to a ScreenScope
+    fn add_program_system<M>(
+        &mut self,
+        prog: impl ProgramLabel + Clone,
+        schedule: impl ScheduleLabel,
+        system: impl IntoSystem<In<Process>, (), M> + 'static,
+    );
+}
+impl ProgramAppExt for App {
+    fn register_program(&mut self, prog: impl ProgramLabel) {
+        self.world_mut().init_resource::<Programs>();
+        self.world_mut()
+            .resource_mut::<Programs>()
+            .0
+            .insert(prog.intern(), ProgramData::default());
+    }
+    fn add_program_system<M>(
+        &mut self,
+        prog: impl ProgramLabel + Clone,
+        schedule: impl ScheduleLabel,
+        system: impl IntoSystem<In<Process>, (), M> + 'static,
+    ) {
+        self.init_resource::<Programs>();
+        let id = self.register_system(system);
+        let mut res = self.world_mut().resource_mut::<Programs>();
+        let data = res.entry(prog.clone()).or_default();
+        data.insert(schedule.intern(), id);
+    }
+}
 
-/// The [`Shell`] which owns this [`Job`].
-#[derive(Component, Reflect, Debug)]
-#[relationship_target(relationship = ShellJob)]
-pub struct ShellJobTarget(Entity);
+pub fn run_programs<S: ScheduleLabel + Default>(
+    mut commands: Commands,
+    q_procs: Query<&Process>,
+    progs: Res<Programs>,
+) {
+    for proc in q_procs.iter() {
+        let pdata = c!(progs.0.get(&proc.prog));
+        let sysid = c!(pdata.get(&S::default().intern()));
+        commands.run_system_with(*sysid, proc.clone());
+    }
+}
 
-/// The focused program. Could be the shell or any other process.
-/// This is the mechanism behind blocking shell input.
-/// All messages sent via [`StdIn`]
-/// **Important:** this should _only_ be set by the shell.
-#[derive(Component, Reflect, Debug)]
-#[relationship(relationship_target = ForegroundJobTarget)]
-pub struct ForegroundProcess(
-    /// The shell for whom this process is the foreground.
-    pub Entity,
-);
+macro_rules! impl_run_progs {
+    ($app:ident, $($sched:ident),+) => {
+        $(
+            $app.add_systems($sched, run_programs::<$sched>);
+        )+
+    };
+}
 
-/// Attached to the [`Shell`] which owns this [`ForegroundJob`]
-#[derive(Component, Reflect, Debug)]
-#[relationship_target(relationship = ForegroundProcess)]
-pub struct ForegroundJobTarget(
-    /// The foreground process.
-    Entity,
-);
+pub fn plugin(app: &mut App) {
+    impl_run_progs!(
+        app,
+        PreUpdate,
+        Update,
+        PostUpdate,
+        FixedPreUpdate,
+        FixedUpdate,
+        FixedPostUpdate
+    );
+}
--- src/data/shell.rs
+++ src/data/shell.rs
@@ -1,7 +1,10 @@
 //! A special [`Process`] used to control other processes.
 
 use crate::prelude::*;
-use bevy::ecs::{lifecycle::HookContext, world::DeferredWorld};
+use bevy::{
+    ecs::{lifecycle::HookContext, world::DeferredWorld},
+    platform::collections::HashMap,
+};
 
 // Kernel equivalent: pty follower.
 // Obviated by single-use.
@@ -10,22 +13,54 @@ use bevy::ecs::{lifecycle::HookContext, world::DeferredWorld};
 #[derive(Component, Reflect, Debug)]
 #[relationship(relationship_target = ShellTarget)]
 #[component(on_add = Self::on_add)]
-pub struct Shell(pub Entity);
+#[require(ForegroundProcessGroup)]
+pub struct Shell {
+    #[relationship]
+    pub term: Entity,
+}
 impl Shell {
     fn on_add(mut world: DeferredWorld, ctx: HookContext) {
         let mut cmds = world.commands();
         cmds.entity(ctx.entity)
             .insert(ForegroundProcess(ctx.entity));
     }
-    /// Spawn a new process. This will foreground the process, placing any other active
-    /// process in the background.
-    pub fn spawn_process<T: Process>(shell: Entity, commands: &mut Commands) -> Entity {
-        let process = commands.spawn((T::default(), ShellJob(shell))).id();
-        Self::foreground_process(shell, process, commands);
-        process
-    }
-    fn foreground_process(shell: Entity, process: Entity, commands: &mut Commands) {
-        commands.entity(process).insert(ForegroundProcess(shell));
+}
+
+#[derive(Message, Debug)]
+pub struct ShellSpawnMsg {
+    prog: InternedProgramLabel,
+    shell: Entity,
+    argv: Vec<String>,
+    environ: HashMap<String, String>,
+}
+
+fn spawn_process(
+    mut commands: Commands,
+    q_terminfo: Query<TermInfo>,
+    q_shell: Query<(Entity, &Shell)>,
+    mut reader: MessageReader<ShellSpawnMsg>,
+) {
+    for msg in reader.read() {
+        let (shell_id, shell) = r!(q_shell.get(msg.shell));
+        let term = r!(q_terminfo.get(shell.term));
+        let term_id = term.id;
+        let mut entt = commands.spawn_empty();
+        let proc_id = entt.id();
+        entt.insert((
+            Process {
+                entity: proc_id,
+                prog: msg.prog,
+                argv: msg.argv.clone(),
+                environ: msg.environ.clone(),
+                fd0: term_id,
+                fd1: term_id,
+                fd2: term_id,
+                signal_overrides: HashMap::new(), // TODO: How??
+            },
+            ShellJob(shell_id),
+            ForegroundProcess(shell_id),
+        ))
+        .id();
     }
 }
 
@@ -38,3 +73,29 @@ impl ShellTarget {
         self.0
     }
 }
+
+/// Marker for a process owned by a [`Shell`].
+#[derive(Component, Reflect, Debug)]
+#[relationship(relationship_target = ShellJobTarget)]
+pub struct ShellJob(pub Entity);
+
+/// The [`Shell`] which owns this [`Job`].
+#[derive(Component, Reflect, Debug)]
+#[relationship_target(relationship = ShellJob)]
+pub struct ShellJobTarget(Entity);
+
+/// This group gets its [stdio](crate::data::io) piped directly to the [`Terminal`].
+/// **Important:** this should _only_ be set by the shell.
+#[derive(Component, Reflect, Debug, Default)]
+#[relationship_target(relationship = ForegroundProcess)]
+pub struct ForegroundProcessGroup {
+    #[relationship_target]
+    processes: Vec<Entity>,
+}
+
+/// Attached to a [`Process`] in the [`ForegroundProcessGroup`].
+/// The inner value is a pointer to a [`Shell`] with an attached
+/// [`ForegroundProcessGroup`]
+#[derive(Component, Reflect, Debug)]
+#[relationship(relationship_target = ForegroundProcessGroup)]
+pub struct ForegroundProcess(pub(crate) Entity);
